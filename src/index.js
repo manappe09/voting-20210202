6302a215136eb3bd5aa47e4a61c0e3f3ea8042bc
manappe09 <manape130@gmail.com>
2021-02-23 05:17:06 +0900

    feat/#13_add polyfill

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import "./js/main";
 import "@fortawesome/fontawesome-free/js/fontawesome";
 import "@fortawesome/fontawesome-free/js/solid";
 import "@fortawesome/fontawesome-free/js/regular";
+import "scroll-behavior-polyfill";
 
 Vue.config.productionTip = false;
 
